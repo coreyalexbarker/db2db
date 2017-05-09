10ade01933185c4cde2616831901feb99a9b3cb9
coreyalexbarker <coreyalexbarker@gmail.com>
2017-05-09 15:40:03 -0400

    added DBFile interface

---
+++ src/main/java/db2db/DBFile.java
@@ -0,0 +1,23 @@
+package db2db;
+
+import okio.Buffer;
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Interface for opening all types of db relation files
+ */
+public interface DBFile extends Closeable {
+  /** Returns the Buffer for this DBFile */
+  Buffer buffer();
+
+  /** Uses fileLocation to create an InputStream that can be passed to Buffer */
+  void setInputFile(String fileLocation) throws IOException;
+
+  /** Uses fileLocation to create an OutputStream that can be passed to Buffer */
+  void setOutputFile(String fileLocation) throws IOException;
+
+  /** Scans directory of input file for related files such as TOAST tables */
+  ArrayList<String> connectedFiles(String fileLocation) throws IOException;
+}
